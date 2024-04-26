4d859689f0470e39194c1d0475893e42f9a35a93
kocean <dev@xanny.us>
2024-04-27 03:13:55 +0900

    fix: header menu

--- components/navbar/header.tsx
+++ components/navbar/header.tsx
@@ -26,7 +26,7 @@ export function Header() {
     <header className="sticky top-0 flex h-16 items-center gap-4 border-b bg-background px-4 md:px-6">
       <nav className="hidden flex-col gap-6 text-lg font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6">
         <Link
-          href="#"
+          href="/"
           className="flex items-center gap-2 text-lg font-semibold md:text-base"
         >
           <Icons.logo className="h-6 w-6" />
@@ -35,35 +35,17 @@ export function Header() {
           </span>
         </Link>
         <Link
-          href="#"
+          href="/"
           className="text-foreground transition-colors hover:text-foreground"
         >
           Dashboard
         </Link>
-        <Link
-          href="#"
-          className="text-muted-foreground transition-colors hover:text-foreground"
-        >
-          Orders
-        </Link>
-        <Link
-          href="#"
-          className="text-muted-foreground transition-colors hover:text-foreground"
-        >
-          Products
-        </Link>
-        <Link
-          href="#"
-          className="text-muted-foreground transition-colors hover:text-foreground"
-        >
-          Customers
-        </Link>
-        <Link
-          href="#"
-          className="text-muted-foreground transition-colors hover:text-foreground"
-        >
-          Analytics
-        </Link>
+        {/*<Link*/}
+        {/*  href={'/setting'}*/}
+        {/*  className="text-muted-foreground transition-colors hover:text-foreground"*/}
+        {/*>*/}
+        {/*  Setting*/}
+        {/*</Link>*/}
       </nav>
 
       <div className="flex w-full items-center gap-4 md:ml-auto md:gap-2 lg:gap-4">
